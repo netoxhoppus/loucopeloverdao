50d3522491a5274325bc11ad2251d81bf2f5ba67
netoxhoppus <netoxhoppus@gmail.com>
2025-02-01 20:55:43 -0300

    Revert "Identação de todos os templates html com IA"
    This reverts commit d4cf00e79226601562fb15ebace709145e67ea77.

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,9 +1,8 @@
 from .models import ConfigImageSite, Post
-
 def conf_imgs(request):
     imgs = ConfigImageSite.objects.all()
     return {"imgs": imgs}
-
+    
 def trending_now(request):
     trending_posts = Post.objects.exclude(id=Post.objects.last().id).order_by('-id')[:3]
     return {"trend":trending_posts}
